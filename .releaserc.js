db82936e1f0739e536de9c2d95937c54da10138e
Robson Oliveira dos Santos <robsonos@outlook.com>
2023-12-19 02:49:48 -0300

    fix: fix releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -65,16 +65,20 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     ['@semantic-release/release-notes-generator', releaseNotesGeneratorOptions],
-    '@semantic-release/changelog',
-    {
-      path: '@semantic-release/npm',
-      npmPublish: true,
-      pkgRoot: '.',
-    },
-    {
-      path: '@semantic-release/git',
-      assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    },
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
   ],
 };
